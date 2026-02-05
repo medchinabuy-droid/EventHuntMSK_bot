cce126891a65249d7fde2095c7eaa0006856145f
medchinabuy-droid <med.china.buy@gmail.com>
2026-02-05 20:43:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from scraper import parse_example
+from scraper import run_all_parsers
 from bot import run_bot
+import threading
+import time
+
+def scheduler():
+    while True:
+        try:
+            run_all_parsers()
+        except Exception as e:
+            print("Parser error:", e)
+        time.sleep(60 * 60 * 6)  # раз в 6 часов
 
 if __name__ == "__main__":
-    parse_example()  # можно потом по расписанию
+    threading.Thread(target=scheduler, daemon=True).start()
     run_bot()
